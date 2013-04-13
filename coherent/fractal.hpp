7d6fcb14ae3c58f111c2022992eb6bf5bc46104e
Noctune <noctune9@gmail.com>
2013-04-13 15:04:24 +0300

    Fixed fractal function scaling and values

--- coherent/fractal.hpp
+++ coherent/fractal.hpp
@@ -4,6 +4,7 @@
 #ifndef COHERENT_FRACTAL_HPP
 #define COHERENT_FRACTAL_HPP
 
+#include <cassert>
 #include <Eigen/Core>
 
 namespace coherent
@@ -18,25 +19,27 @@ namespace coherent
 	///
 	/// 'persistence' is the the proportional change in amplitude (a value of
 	/// 0.5 will give each octave half the amplitude of the one before it).
+	/// This value must not be equal to 1.
 	///
 	/// 'position' is the position of the noise value to return.
-	///
-	/// BROKEN AT THE MOMENT: Non-deterministic, often returning 0.
 	template <typename Iterator, typename Derived>
 	typename Derived::Scalar fractal(Iterator begin,
 	                                 Iterator end,
 	                                 typename Derived::Scalar lacunarity,
 	                                 typename Derived::Scalar persistence,
-	                                 Eigen::MatrixBase<Derived> position)
+	                                 const Eigen::MatrixBase<Derived>& position)
 	{
 		EIGEN_STATIC_ASSERT_VECTOR_ONLY(Derived);
+		
+		//A persistence of 1 is not supported. Instead, use a value close to 1.
+		assert(persistence != 1.0);
 		
 		typename Derived::Scalar sum = 0, per = 1, lac = 1;
 		for (Iterator i = begin; i != end; i++, per *= persistence, lac *= lacunarity)
 			sum += (*i)(position * lac) * per;
 		
-		//Apply scaling factor (preliminary, might not be correct)
-		return sum; // * (persistence - 1) / (per - 1); TODO: fix scaling
+		//Apply scaling factor
+		return sum * (persistence - 1) / (per - 1);
 	}
 }
 #endif
