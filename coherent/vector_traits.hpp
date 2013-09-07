b2e81a11213f65a5cdf04d2e4d7c06662264e7ae
Mike Pedersen <noctune9@gmail.com>
2013-09-07 15:03:12 +0200

    Added vector traits

---
+++ coherent/vector_traits.hpp
@@ -0,0 +1,46 @@
+#ifndef COHERENT_VEC_INFO_HPP
+#define COHERENT_VEC_INFO_HPP
+
+#include <type_traits>
+#include <glm/glm.hpp>
+
+namespace coherent
+{
+	namespace detail
+	{
+		template <typename VectorType>
+		struct length
+		{
+			static_assert(false, "Length can only be used with glm vector types");
+		};
+	
+		template <typename ScalarType>
+		struct length<glm::detail::tvec1<ScalarType>> : std::integral_constant<int, 1> { };
+		template <typename ScalarType>
+		struct length<glm::detail::tvec2<ScalarType>> : std::integral_constant<int, 2> { };
+		template <typename ScalarType>
+		struct length<glm::detail::tvec3<ScalarType>> : std::integral_constant<int, 3> { };
+		template <typename ScalarType>
+		struct length<glm::detail::tvec4<ScalarType>> : std::integral_constant<int, 4> { };
+	
+		template <typename VectorType>
+		struct is_vector : std::false_type { }
+		template <typename ScalarType>
+		struct is_vector<glm::detail::tvec1<ScalarType>> : std::true_type { };
+		template <typename ScalarType>
+		struct is_vector<glm::detail::tvec2<ScalarType>> : std::true_type { };
+		template <typename ScalarType>
+		struct is_vector<glm::detail::tvec3<ScalarType>> : std::true_type { };
+		template <typename ScalarType>
+		struct is_vector<glm::detail::tvec4<ScalarType>> : std::true_type { };
+	
+		template <typename Type>
+		constexpr void assert_vector()
+		{
+			static_assert(is_vector<Type>, "Must be a vector type");
+		};
+	}
+}
+
+#endif // COHERENT_VEC_INFO_HPP
+
