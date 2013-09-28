33a7e9c386e4bb08c7cfafbea29d70b6ec8f7f52
Mike Pedersen <noctune9@gmail.com>
2013-09-28 13:53:43 +0200

    Added scalar check

--- generative/vector_traits.hpp
+++ generative/vector_traits.hpp
@@ -9,35 +9,35 @@ namespace generative
 	namespace detail
 	{
 		template <typename VectorType>
-		struct length
-		{
-			//static_assert(false, "Length can only be used with glm vector types");
-		};
-	
-		template <typename ScalarType>
-		struct length<glm::detail::tvec1<ScalarType>> : std::integral_constant<int, 1> { };
-		template <typename ScalarType>
-		struct length<glm::detail::tvec2<ScalarType>> : std::integral_constant<int, 2> { };
-		template <typename ScalarType>
-		struct length<glm::detail::tvec3<ScalarType>> : std::integral_constant<int, 3> { };
-		template <typename ScalarType>
-		struct length<glm::detail::tvec4<ScalarType>> : std::integral_constant<int, 4> { };
-	
-		template <typename VectorType>
 		struct is_vector : std::false_type { };
-		template <typename ScalarType>
-		struct is_vector<glm::detail::tvec1<ScalarType>> : std::true_type { };
-		template <typename ScalarType>
-		struct is_vector<glm::detail::tvec2<ScalarType>> : std::true_type { };
-		template <typename ScalarType>
-		struct is_vector<glm::detail::tvec3<ScalarType>> : std::true_type { };
-		template <typename ScalarType>
-		struct is_vector<glm::detail::tvec4<ScalarType>> : std::true_type { };
-	
+		template <> struct is_vector<glm::vec2>  : std::true_type { };
+		template <> struct is_vector<glm::vec3>  : std::true_type { };
+		template <> struct is_vector<glm::vec4>  : std::true_type { };
+		template <> struct is_vector<glm::dvec2> : std::true_type { };
+		template <> struct is_vector<glm::dvec3> : std::true_type { };
+		template <> struct is_vector<glm::dvec4> : std::true_type { };
+		
+		template <typename VectorType>
+		struct is_scalar : std::false_type { };
+		template <> struct is_scalar<float>  : std::true_type { };
+		template <> struct is_scalar<double> : std::true_type { };
+		
 		template <typename Type>
 		struct assert_vector
 		{
 			static_assert(is_vector<Type>::value, "Must be a vector type");
+		};
+		
+		template <typename Type>
+		struct assert_scalar
+		{
+			static_assert(is_scalar<Type>::value, "Must be a scalar type");
+		};
+		
+		template <typename Type>
+		struct assert_vector_or_scalar
+		{
+			static_assert(is_vector<Type>::value || is_scalar<Type>::value, "Must be a vector or scalar type");
 		};
 	}
 }
